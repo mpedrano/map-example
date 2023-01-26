aa8f8f55937958c5ba3b9ab29fb3fa159ca02914
mpedrano <mpedrano@my.bcit.ca>
2023-01-26 15:59:17 -0800

    fix

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import data from '../data/education.json'
 import { useState } from 'react'
-import Card from '@../components/Card/index.js'
+import Card from '@/components/card/index.js'
 import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
